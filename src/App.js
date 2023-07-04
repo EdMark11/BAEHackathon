9fb46421b6930b1b656d4dd46534ec1e01105184
BenPuusta <benpuusta@gmail.com>
2023-07-04 13:12:30 +0100

    new branch created

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@
 function App() {
   return (
     <div>
-      <h1 className="text-center text-3xl">MyIntensity</h1>
+      <h1 className="text-center text-3xl">MyIntensity </h1>
     </div>
   );
 }
