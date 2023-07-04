26cf5a18b78936583cd51c6e4e1c8df6a7d340c4
BenPuusta <benpuusta@gmail.com>
2023-07-04 13:35:31 +0100

    embryonic fetch request made

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
+import React, {useState} from "react";
+
 
 
 function App() {
+
+  const [testResponse, setTestResponse] = useState('test')
+
+async function fetchStuff() {
+  const response = await fetch(
+    "https://api.carbonintensity.org.uk/regional/postcode/B30?auth-token=X9N1x3zDUYCdCEMCy6OKgE58nS3P3IBs"
+  );
+  let data = await response.json();
+ setTestResponse(data)
+ console.log(testResponse)
+}
+
+
+
   return (
     <div>
-      <h1 className="text-center text-3xl">MyIntensity </h1>
+      <h1 className="text-center text-3xl">MyIntensity OIOKOKOK</h1>
+   
+     <button onClick={()=>{fetchStuff()}}>HELP ME</button>
     </div>
   );
 }
